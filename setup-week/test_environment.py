3bb6bd67ab9547d9474252594c647efa546e9245
Magdalena McCrimmon <magda.mccrimmon@gmail.com>
2026-01-19 14:45:20 +0000

    Setup Week Day 1: Python environment tested and verified

---
+++ setup-week/test_environment.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import seaborn as sns
+
+print('✓ All packages working!')
+print(f'pandas version: {pd.__version__}')
+print(f'numpy version: {np.__version__}')
+print(f'matplotlib version: {matplotlib.__version__}')
+print(f'seaborn version: {sns.__version__}')
